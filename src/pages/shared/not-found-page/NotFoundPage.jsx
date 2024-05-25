1b9ea4bf03277d39beb6a87b65f52581afb178c0
BarkinSayin <barkinsayin@hotmail.com>
2024-05-25 14:03:00 +0300

    redirect with comments

--- src/pages/shared/not-found-page/NotFoundPage.jsx
+++ src/pages/shared/not-found-page/NotFoundPage.jsx
@@ -2,17 +2,22 @@ import { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 const NotFoundPage = () => {
+  //Timer için bir useState oluşturduk
   const [count, setCount] = useState(5);
-
+  //Belirtilen sayfaya yönlendirmesi için kullanılıyor
   const navigate = useNavigate();
-
+  //Count her güncellendiğinde fonksiyonun çalışması için useEffect oluşturuyoruz
   useEffect(() => {
     if (count === 0) {
+      //count 0'a eşit olunca navigate sayfaya yönlendiriyor
       navigate("/");
-    } else
+    }
+    //1000 milisaniyeden sonra setCount çalışıyor
+    else
       setTimeout(() => {
         setCount(count - 1);
       }, 1000);
+    //Her count güncellendiğinde useEffect bir daha çalışıyor
   }, [count]);
 
   return (
